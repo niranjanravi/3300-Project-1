e316f694dd00b0b5a45fcea41c09bca424cdaccc
Michael Gingras <mcg79@cornell.edu>
2017-02-28 09:11:01 -0500

    preprocessing data into artists, python script to scrape big lyric file

---
+++ getsongs.py
@@ -0,0 +1,19 @@
+song_list = []
+
+def preprocess_corpus(save_as, artist):
+	with open('songdata.csv') as f:
+		contents = f.read()
+
+	split = contents.split('"')
+	print split[4]
+
+	for index, song in enumerate(split):
+		if (index % 2 == 1 and artist in song) :
+			song_list.append(split[index+1])
+
+	with open(save_as,'w') as out:
+		for song in song_list:
+			out.write(song)
+
+
+preprocess_corpus("data/drake.txt",drake)
