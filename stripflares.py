68fbf4648a67a85cb478264fe4d7b821d08dba77
Charles Yu <charles.yu108@gmail.com>
2017-03-01 11:51:05 -0500

    added stripping code and stripped-lyrics

---
+++ stripflares.py
@@ -0,0 +1,37 @@
+import re
+import glob
+import os
+datasource = "data/*"
+
+
+bracketRE = r"^\[[\w|\s|:|'|-|\.|\$]+\]" #Handles things like [Kanye], [Outro: x3], etc
+colonRE = r"^[\w|\s|:|'|-|\.|\$]+:" #Handles expressions like 'Gucci Mane:' at the begging of new lines
+parenthesesRE = r"^\([\w|\s|:|'|-| \.| \$ | \?]+\)" #Handles expressions at beginnng of new line like '(Gucci)'
+#Does catch some lyrics... but at least putting it at begging of new line minimizes some of that
+
+newpath = 'stripped-lyrics/'
+
+if not os.path.exists(newpath):
+	os.mkdir(newpath)
+
+for filename in glob.iglob(datasource):
+	name = filename[len(datasource)-1:]
+	lyrics = """ """
+	with open(filename, 'r') as f:
+		for line in f:
+			lyrics += line
+
+	f.close()
+	lyrics = re.sub(bracketRE, "", lyrics, flags = re.MULTILINE)
+	lyrics = re.sub(colonRE, "", lyrics, flags = re.MULTILINE)
+	lyrics = re.sub(parenthesesRE, "", lyrics, flags = re.MULTILINE)
+
+	newfilepath = newpath + "stripped-" + name
+
+	if os.path.exists(newfilepath):
+		os.remove(newfilepath)
+
+	with open(newpath + "stripped-" + name, 'w') as new:
+		new.write(lyrics)
+	new.close()
+	print("Stripped " + name + " as " + newfilepath)
